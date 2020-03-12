8a6c9450774bc0390199cb0fd3984842efca4794
Bruno Mokross <bruno.mokross@gmail.com>
2020-03-12 15:20:59 +0100

    build again

--- site/config/config.php
+++ site/config/config.php
@@ -6,7 +6,7 @@ return [
         'endpoint' => 'generate-static-site', # set to any string like 'generate-static-site' to use the built-in endpoint (necessary when using the blueprint field)
         'output_folder' => './docs', # you can specify an absolute or relative path
         'preserve' => [], # preserve individual files / folders in the root level of the output folder (anything starting with "." is always preserved)
-        'base_url' => '/kirby-static-test/docs/', # if the static site is not mounted to the root folder of your domain, change accordingly here
+        'base_url' => '/kirby-static-test/', # if the static site is not mounted to the root folder of your domain, change accordingly here
         'skip_media' => false, # set to true to skip copying media files, e.g. when they are already on a CDN; combinable with 'preserve' => ['media']
         'skip_templates' => [] # ignore pages with given templates (home is always rendered)
       ]
